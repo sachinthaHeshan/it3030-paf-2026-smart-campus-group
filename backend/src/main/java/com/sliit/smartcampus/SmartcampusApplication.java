a2cf9e5962f8ca6452922ca9ac8d1171942c0f49
Sachintha Heshan <67828426+sachinthaHeshan@users.noreply.github.com>
2026-03-01 19:33:26 +0530

    Initialize backend project

---
+++ backend/src/main/java/com/sliit/smartcampus/SmartcampusApplication.java
@@ -0,0 +1,16 @@
+package com.sliit.smartcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcampusApplication.class, args);
+
+		System.out.println("Hello World");
+		
+	}
+
+}
